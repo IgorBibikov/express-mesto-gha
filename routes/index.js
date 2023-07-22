92cc0c367e5dbdf1353b9abfb605c78f75ff0125
Igor Bibikov <igb604@yandex.ru>
2023-07-22 13:17:51 +0500

    создан сервер5

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ const { cardsRoutes } = require('./cards');
 routes.use('/users', usersRoutes);
 routes.use('/cards', cardsRoutes);
 
+routes.use('/', (req, res, next) => {
+  next(res.status(404).send({ message: 'Неверный путь' }));
+});
+
 module.exports = { routes };
